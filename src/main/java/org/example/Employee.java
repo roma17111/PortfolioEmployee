49ae548a8738b1a6ecb15ea1466a6be37dc80869
roman <romanze1706@gmail.com>
2023-02-12 16:05:14 +0300

    Class Employee

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,7 +1,32 @@
 package org.example;
 
+
+import lombok.*;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@NonNull
 public class Employee {
+    private static int count;
 
-    private String fullName;
+    private int id;
+    private final String fullName;
+    private Department department;
+    private int salary;
 
+    public Employee(String fullName, Department department, int salary) {
+        this.id = count++;
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудник" +
+                "ФИО" + fullName + '\n' +
+                "Отдел: " + department +'\n' +
+                "Зарплата: " + salary +'\n';
+    }
 }
