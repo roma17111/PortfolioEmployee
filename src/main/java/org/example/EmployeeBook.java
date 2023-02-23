09683743b6ddffbfe47480a6d077ff4ac2c492fe
roman <romanze1706@gmail.com>
2023-02-23 12:10:05 +0300

    ...

--- src/main/java/org/example/EmployeeBook.java
+++ src/main/java/org/example/EmployeeBook.java
@@ -156,4 +156,42 @@ public class EmployeeBook {
                 .filter(employee -> employee.getSalary() >= n)
                 .forEach(System.out::println);
     }
+
+    public static void testEmployeeBook() {
+        EmployeeBook employeeBook = new EmployeeBook();
+        employeeBook.addEmployee(new Employee("Якименко Роман Эдуардович",
+                1,
+                200000));
+        employeeBook.addEmployee(new Employee("Якименко Вероника Владимировна",
+                2,
+                150000));
+        employeeBook.addEmployee(new Employee("Васин иван иванович",
+                3,
+                120000));
+        employeeBook.addEmployee(new Employee("Алёшина Наталья Михайловна",
+                4,
+                80000));
+        employeeBook.addEmployee(new Employee("Иванова Марина борисовна",
+                5,
+                95000));
+        employeeBook.addEmployee(new Employee("Андреев Сергей Викторович",
+                1,
+                175000));
+        employeeBook.addEmployee(new Employee("Изотова Римма Андреевна",
+                2,
+                65000));
+        employeeBook.addEmployee(new Employee("Костин Сергей Витальевич",
+                3,
+                98000));
+        employeeBook.addEmployee( new Employee("Катина Ирина Яковлевна",
+                4,
+                42000));
+        employeeBook.addEmployee(new Employee("Яковенко Владлена Игоревна",
+                5,
+                52000));
+        employeeBook.changeEmployeeByFio("Якименко Роман Эдуардович", 1000200, 5);
+        employeeBook.printAllEmployees();
+        employeeBook.getSumAllSallaryes();
+        employeeBook.printAllEmployeesBYDepartments();
+    }
 }
---
+++ src/main/java/org/example/service/ScannerService.java
@@ -0,0 +1,12 @@
+package org.example.service;
+
+import java.util.Scanner;
+
+public class ScannerService {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void startMenu() {
+    }
+
+}
